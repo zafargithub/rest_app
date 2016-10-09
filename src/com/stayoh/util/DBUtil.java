3be6c45f39520841f8f6d92c3596bba4dc1e24b7
vijay tiwari <vijay_evill@yahoo.com>
2016-10-09 23:06:18 +0530

    Test commit by Vijay
    Test commit by Vijay

--- src/com/stayoh/util/DBUtil.java
+++ src/com/stayoh/util/DBUtil.java
@@ -11,6 +11,7 @@ public class DBUtil {
 	private static final String USERNAME = "";
 	private static final String URL = "";
 	private static final String PASSWORD = "";
+	private static final String TEST = "";
 	
 	static{
 		try{
